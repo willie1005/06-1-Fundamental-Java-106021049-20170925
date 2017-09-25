320fda3825ced9257661ac1fb60c15390c1a9dae
willie1005 <willie888699@yahoo.com.tw>
2017-09-25 15:18:45 +0800

    20170925

---
+++ 01.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int val1 = scn.nextInt();
+        float val2 = (float) 0.26418;
+        System.out.println(Math.round(val1*val2*10)/10f);
+}
+}
