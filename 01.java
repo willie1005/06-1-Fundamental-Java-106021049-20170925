8d5b57743a18ef979f94135fc0cf08be2542e3dd
willie1005 <willie888699@yahoo.com.tw>
2017-09-25 16:15:55 +0800

    20170925

--- 01.java
+++ 01.java
@@ -11,19 +11,3 @@ public class Main {
         float val2 = (float) 0.26418;
         System.out.println(Math.round(val1*val2*10)/10f);
 }
-}
-=======
-package com.company;
-
-import java.util.Scanner;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        int val1 = scn.nextInt();
-        float val2 = (float) 0.26418;
-        System.out.println(Math.round(val1*val2*10)/10f);
-}
-}
->>>>>>> origin/master
